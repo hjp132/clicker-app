abbf7a76b844a06bc8d8f9ba9d9850c66d18dd4c
harry <harrypalmer889@gmail.com>
2020-09-28 12:54:48 +0100

    removing redundant component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Text, StyleSheet, View, Button, Dimensions, TouchableOpacity, ScrollView } from "react-native";
-import UpgradeScreen from '../components/UpgradeScreen';
-
+import { Text, StyleSheet, View, Dimensions, TouchableOpacity, ScrollView } from "react-native";
 import UpgradeItem from '../components/UpgradeItem';
 
 
@@ -52,26 +50,6 @@ const HomeScreen = (props) => {
       return false
     }
   }
-
-  // Can't seem to get this to work 
-  // function abbreviateNumber(value) {
-  //   var newValue = value;
-  //   if (value >= 1000) {
-  //     var suffixes = ["", "k", "m", "b", "t"];
-  //     var suffixNum = Math.floor(("" + value).length / 3);
-  //     var shortValue = '';
-  //     for (var precision = 2; precision >= 1; precision--) {
-  //       shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000, suffixNum)) : value).toPrecision(precision));
-  //       var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g, '');
-  //       if (dotLessShortValue.length <= 2) { break; }
-  //     }
-  //     if (shortValue % 1 != 0) shortValue = shortValue.toFixed(1);
-  //     newValue = shortValue + suffixes[suffixNum];
-  //   }
-  //   return setClickNumber(newValue);
-  // }
-
-
 
   return (
     <View style={styles.container}>
